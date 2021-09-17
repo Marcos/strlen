2b9ccaf2fe1211be4ecfff1c40775827507a839b
marcos <marcos.ferreira@mercadolivre.com>
2021-09-17 14:03:56 -0300

    Improve verbose option to print total by chars

--- main.go
+++ main.go
@@ -4,8 +4,33 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"sort"
 	"strconv"
 )
+
+func showVerboseResult(str string) {
+	chars := make(map[byte]int)
+
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+		totalCount, hasChar := chars[char]
+		count := 1
+		if hasChar {
+			count = totalCount + 1
+		}
+		chars[char] = count
+	}
+
+	keys := make([]string, 0, len(chars))
+	for k := range chars {
+		keys = append(keys, string(k))
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Println(k + ": " + strconv.Itoa(chars[[]byte(k)[0]]))
+	}
+}
 
 func main() {
 	verbose := flag.Bool("v", false, "Display verbose size")
@@ -20,6 +45,7 @@ func main() {
 
 	var buffer bytes.Buffer
 	if *verbose {
+		showVerboseResult(str)
 		buffer.WriteString(str + ": ")
 	}
 
